8b80b32a9220c91315b0eea2160c7c6535402fe9
Arturo Diaz <luisarturodiazmeza_abc@hotmail.com>
2021-08-27 04:51:32 -0500

    1commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootswatch/dist/lux/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
